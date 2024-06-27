0197966557813752eb0238ebd4ac70377b28082f
Eichi2514 <picon1317@gmail.com>
2024-06-27 13:41:10 +0900

    기초1

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,7 +1,18 @@
 package org.koreait;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        for (int i = 0; i < b; i++) {
+            for (int x = 0; x < a; x++) {
+                System.out.printf("*");
+            }
+            System.out.println(" ");
+        }
     }
 }
