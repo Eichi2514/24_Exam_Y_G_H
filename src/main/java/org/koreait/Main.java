ce01a967416915bf6258feb3800f7ed722102cfe
Eichi2514 <picon1317@gmail.com>
2024-06-27 14:07:35 +0900

    기초2

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -6,12 +6,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
-        int b = in.nextInt();
 
-        for (int i = 0; i < b; i++) {
-            for (int x = 0; x < a; x++) {
-                System.out.printf("*");
+        int b = a;
+        for (int i = 0; i <= a; i++) {
+            for (int x = 0; x < i; x++) {
+                System.out.printf(" ");
             }
+            for (int x1 = b; x1 > 0; x1--) {
+                System.out.printf("%s ", x1);
+            }
+            b--;
             System.out.println(" ");
         }
     }
